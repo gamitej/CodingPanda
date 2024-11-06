4d01d6b82e089e6b2f5c0edddda439a8c09cb2f5
gamitej <gamitejpratapsingh998@gmail.com>
2024-11-06 23:17:30 +0530

    NEW : home page navbar

--- src/pages/explore/common/navbar/index.tsx
+++ src/pages/explore/common/navbar/index.tsx
@@ -1,5 +1,31 @@
+import { Link } from "react-router-dom";
+
 const ExploreNavbar = () => {
-  return <div>ExploreNavbar</div>;
+  /**
+   * TSX
+   */
+  return (
+    <div className="flex justify-between items-center h-[4rem] px-12 bg-white border-b">
+      <div>
+        <Link
+          to="/"
+          className="text-xl text-gray-700 font-poppins"
+          style={{ fontWeight: 600 }}
+        >
+          Coding
+          <span className="text-blue-600 ml-[1px]">Panda</span>
+        </Link>
+      </div>
+      <div>
+        <ul className="flex gap-6 items-center text-gray-700 text-md">
+          <Link to="online-ide" className="cursor-pointer">
+            Online IDE
+          </Link>
+          <li className="cursor-pointer">Problems</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default ExploreNavbar;
